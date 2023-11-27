0334d3ce8f8c0596fbfcb2b6751339db243199a4
Tapiwanashe92 <tapiwanashembk@gmail.com>
2023-11-27 08:11:04 -0800

    SSH Configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-# using puppet to make changes to our configuration file
+# connect to a server Execute a command
 
-file { 'etc/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	"
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
